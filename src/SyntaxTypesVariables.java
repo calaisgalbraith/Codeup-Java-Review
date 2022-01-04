74d87f13a6ae8ec563c0890e613e48fe357faf2f
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-04 10:27:21 -0600

    finished syntax, types, & variables exercises

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,30 @@
+public class SyntaxTypesVariables {
+
+    //Exercises from Syntax, Types, Variables module
+
+    public static void main(String[] args) {
+
+        //Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 14;
+        System.out.println(myFavoriteNumber);
+
+        //Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+        String myString = "Calais";
+        System.out.println(myString);
+
+        //Rewrite the following expressions using the relevant shorthand assignment operators:
+
+            int x = 4;
+            x+= 5;           //x = x + 5;
+
+            int z = 3;
+            int y = 4;
+            y *= z;          //y = y * x;
+
+            int a = 10;
+            int b = 2;
+            a /= b;          //a = a / b;
+            b -= a;          //b = b - a;
+
+    }
+}
