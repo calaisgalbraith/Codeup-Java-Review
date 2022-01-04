cab910d3444635463031000c52e9faea68077f3e
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-04 11:01:25 -0600

    finished fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -55,8 +55,32 @@ public class ControlFlowExercises {
             }
     }
 
+
+    //Write a program that prints the numbers from 1 to 100.
+    //For multiples of three: print “Fizz” instead of the number.
+    //For the multiples of five: print “Buzz”.
+    //For numbers which are multiples of both three and five: print “FizzBuzz”.
+    public static void fizzBuzz(){
+        for(int i = 1; i <= 100; i++){
+
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         loopBasics();
+        fizzBuzz();
 
 
 
