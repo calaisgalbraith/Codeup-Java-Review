580455044c41407fafdc70dd890aea1e59507cfb
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-04 10:20:51 -0600

    finished HelloWorld exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,15 @@
 public class HelloWorld {
+
+    //Create a program that outputs "Hello, World!" to the console.
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        //Refactor your code to use two System.out.print statements without changing the output.
+
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+    }
 }
