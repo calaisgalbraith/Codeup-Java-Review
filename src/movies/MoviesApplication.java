8afdbefa9d6ce6502964c07772b373a4008e0ab2
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-10 12:25:13 -0600

    started movieList exericse - WIP

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,18 @@
+package movies;
+
+//Give the user a list of options for viewing all the movies or viewing movies by category.
+//Use your Input class to get input from the user, and display information based on their choice.
+//If a category is selected, only movies from that category should be displayed.
+//Your application should continue to run until the user chooses to exit.
+//Bonus: Add functionality to allow a user to add a new movie to the list.
+
+public class MoviesApplication {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
