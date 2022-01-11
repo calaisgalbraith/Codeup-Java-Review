24bea5f26a9df3a510927b02c48574bc9f87f903
calaisgalbraith <calaisgalbraith@gmail.com>
2022-01-10 22:48:37 -0600

    test commit

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,5 +1,6 @@
 package movies;
 
+//
 //Give the user a list of options for viewing all the movies or viewing movies by category.
 //Use your Input class to get input from the user, and display information based on their choice.
 //If a category is selected, only movies from that category should be displayed.
